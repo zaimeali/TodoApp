a8572a2aa1081c8c70991bcb7e36431f8bd8e6b3
zaime ali <zaimeali1997@gmail.com>
2020-07-21 00:34:40 +0500

    Create New Task Done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Ref about DOM
 1) https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Event_handlers
 2) https://stackoverflow.com/questions/44246103/get-parent-div-id-by-clicking-on-a-child-element
+3) https://stackoverflow.com/questions/6900124/how-to-make-certain-text-not-selectable-with-css
--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 let myTodo = [
-    {
-        'id': 1,
-        'task': 'Bring Milk',
-        'isComplete': false
-    },
-    {
-        'id': 2,
-        'task': 'Bring Biscuit',
-        'isComplete': true
-    },
+    // {
+    //     'id': 1,
+    //     'task': 'Bring Milk',
+    //     'isComplete': false
+    // },
+    // {
+    //     'id': 2,
+    //     'task': 'Bring Biscuit',
+    //     'isComplete': true
+    // },
 ];
 
 function refreshTodo() {
     let myList = document.getElementById('toDo');
+
+    document.getElementById('toDo').innerHTML = '';
 
     myTodo.map(({id, task, isComplete}, i) => {
         let list = document.createElement("li");
@@ -22,18 +24,29 @@ function refreshTodo() {
         clickable.className = "btn btn-custom";
         clickable.setAttribute("onclick", `updateTodo(event, ${i})`);
         list.appendChild(clickable);
-        list.className = isComplete ? "done" : "notDone";
+        list.className = `list_task ${isComplete ? "done" : "notDone"}`;
         myList.appendChild(list);
     });
 }
 
-function updateTodo({target}, i){
+function updateTodo({ target }, i){
     console.log(target.id, i);
     let flag = myTodo[i].isComplete;
     myTodo[i].isComplete = !flag;
     console.log(myTodo[i]);
     let parent = document.getElementById(target.id).parentElement;
-    parent.className = myTodo[i].isComplete ? "done" : "notDone";
+    parent.className = `list_task ${myTodo[i].isComplete ? "done" : "notDone"}`;
 }
 
-
+function createTodo({ target }){
+    console.log(target.value);
+    let newTodo = {
+        'id': myTodo.length + 1,
+        'task': target.value,
+        'isComplete': false
+    };
+    target.value = '';
+    console.log(newTodo);
+    myTodo = [...myTodo, newTodo];
+    refreshTodo();
+}
